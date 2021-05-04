cc703c17208f59685ec6eb4ea5897954582aa016
Daichi Kitamura <d-kitamura@ieee.org>
2021-05-04 22:42:26 +0900

    renew STFT and ISTFT (more efficient)

--- example_STFTandISTFT.m
+++ example_STFTandISTFT.m
@@ -14,7 +14,7 @@ close all; % close all plot figures
 wavPath1 = sprintf('./input/drums.wav'); % file path of wav signal
 wavPath2 = sprintf('./input/piano.wav'); % file path of wav signal
 
-% Audio read
+% Read audio files
 [s1,fs] = audioread(wavPath1); % fs: sampling frequency [Hz], s1 is a vector of size "length x channels"
 [s2,fs] = audioread(wavPath2); % s1, s2, and s3 are column vectors because sample wave files are monaural
 
@@ -22,27 +22,26 @@ wavPath2 = sprintf('./input/piano.wav'); % file path of wav signal
 SNR = 0;
 [x,s1,s2,coef] = SNRmix(s1,s2,SNR); % mixture signal of size "1 x length"
 
-% Apply short-time Fourier transform
+% Apply short-time Fourier transform (STFT)
 fftSize = 2048;
 shiftSize = fftSize/4;
-window = 'hamming';
-[S1,analyWin] = STFT(s1,fftSize,shiftSize,window);
-[S2,analyWin] = STFT(s2,fftSize,shiftSize,window);
-[X,analyWin] = STFT(x,fftSize,shiftSize,window);
+winType = 'hamming';
+[S1,analyWin,orgLen1] = STFT(s1,fftSize,shiftSize,winType);
+[S2,analyWin,orgLen2] = STFT(s2,fftSize,shiftSize,winType);
+[X,analyWin,orgLenX] = STFT(x,fftSize,shiftSize,winType);
 
 % Show spectrograms
-showSpect(S1,fs,shiftSize);
-showSpect(S2,fs,shiftSize);
-showSpect(X,fs,shiftSize);
+showSpectLite(S1,fs,shiftSize);
+showSpectLite(S2,fs,shiftSize);
+showSpectLite(X,fs,shiftSize);
 
-% Apply inverse short-time Fourier transform
-orgLength = size(s1,1);
-y1 = ISTFT(S1,shiftSize,analyWin,orgLength);
-y2 = ISTFT(S2,shiftSize,analyWin,orgLength);
-z = ISTFT(X,shiftSize,analyWin,orgLength);
+% Apply inverse STFT (ISTFT)
+y1 = ISTFT(S1,shiftSize,analyWin,orgLen1);
+y2 = ISTFT(S2,shiftSize,analyWin,orgLen2);
+z = ISTFT(X,shiftSize,analyWin,orgLenX);
 
 % Numerical error caused by calculations in STFT and ISTFT
 err1 = sum((s1-y1).^2)
-err2 = sum((s1-y1).^2)
+err2 = sum((s2-y2).^2)
 err3 = sum((x-z).^2)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EOF %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
